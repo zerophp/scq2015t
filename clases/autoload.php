0ebec1d464d920076ea6b501a53ebcfbe94fa8c1
Agustin Calderon <agustincl@gmail.com>
2015-04-27 17:50:56 +0200

    dispatch todo

--- clases/autoload.php
+++ clases/autoload.php
@@ -1,41 +1,48 @@
 <?php
 
-function autoload($className)
+
+
+function autoload($classname)
 {
-    $className = ltrim($className, '\\');
-    $fileName  = '';
-    $namespace = '';
-    if ($lastNsPos = strrpos($className, '\\')) {
-        $namespace = substr($className, 0, $lastNsPos);
-        $className = substr($className, $lastNsPos + 1);
-        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-    }
-    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-
-    require $fileName;
-}
-spl_autoload_register('autoload');
-
-
-function __autoload($classname)
-{
-    echo "<pre>autoload: ";
-    print_r($classname);
-    echo "</pre>";
+//     echo "<pre>autoload: ";
+//     print_r($classname);
+//     echo "</pre>";
     $data = explode("\\",$classname);
     
-    echo "<pre>autoload: ";
-    print_r($data);
-    echo "</pre>";
+//     echo "<pre>autoload: ";
+//     print_r($data);
+//     echo "</pre>";
     
-//     echo  __DIR__."\\Modules\\Application\\src\\".$classname.".php";
-    die;
+//     die;
     $filename = __DIR__."\\Modules\\".$data[0]."\\src\\".$classname.".php";
-    $vendorame = __DIR__."\\Vendor\\".$data[0]."\\src\\".$classname.".php";
+   
+   
+    
     if(file_exists($filename))
         include $filename;
-    else
-        include $vendorame;
+    else 
+    {
+        $vendorname = __DIR__."\\Vendor\\".$data[0]."\\".$data[1]."\\src\\".$data[1]."\\".$data[2].".php";
+//         echo $vendorname;
+        if (file_exists($vendorname))
+            include $vendorname;
         
+    }
+        
+    return;
+   
     
 }
+
+
+function libraryAutoload()
+{
+    echo "library";
+}
+
+
+
+spl_autoload_register('autoload');
+spl_autoload_register('libraryAutoload');
+
+
--- clases/public/index.php
+++ clases/public/index.php
@@ -1,58 +1,17 @@
 <?php 
 
-use \Application\Controller\kakita;
-
 include "../autoload.php";
-
-$kaka = new kakita();
-
-
-
-die;
-
-
 define ("ROOT_PATH", __DIR__.'/..');
 define ("APPLICATION_PATH", __DIR__.'/../modules/Application/src/Application');
-define ("CRUD_PATH", __DIR__.'/../modules/Crud/src/Crud');
-define ("VENDOR_PATH", __DIR__.'/../vendor');
 
-$config = readConfig(ROOT_PATH.'/configs/application.config.php');
 
-$router = readRouter(explode("/",$_SERVER['REQUEST_URI'])[1]);
+use acl\Zerophp\Router;
+use acl\Zerophp\Config;
+use acl\Zerophp\Dispatch;
 
-// echo "<pre>";
-// print_r($router);
-// echo "</pre>";
 
-$request = parseUrl($_SERVER['REQUEST_URI'], $router);
-
-// echo "<pre>";
-// print_r($config);
-// echo "</pre>";
-
-switch($request['controller'])
-{
-
-    case 'users':
-        include (APPLICATION_PATH."/Controller/Users.php");
-    break;
-    
-    case 'index':
-    case 'home':
-        include (APPLICATION_PATH."/Controller/Home.php");
-    break;
-    
-    case 'crud':
-        include (CRUD_PATH."/Controller/Crud.php");
-        break;
-    
-    case 'error':
-        echo "<pre>";
-        print_r($request);
-        echo "</pre>";
-        die("error");
-    break;
-    
-}
-
+$config = Config::readConfig(__DIR__.'/../configs/application.config.php');
+$router = Router::readRouter(explode("/",$_SERVER['REQUEST_URI'])[1]);
+$request = Router::parseUrl($_SERVER['REQUEST_URI'], $router);
+Dispatch::dispatch($request);
 
